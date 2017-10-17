f6bdb5645c36a373318d6425dc0a3dbf87be4e2a
isabello <isabella@lightcurve.io>
2017-10-17 12:59:48 +0200

    Update libsodium to 1.0.15

--- deps/libsodium/src/libsodium/crypto_scalarmult/curve25519/scalarmult_curve25519.c
+++ deps/libsodium/src/libsodium/crypto_scalarmult/curve25519/scalarmult_curve25519.c
@@ -1,5 +1,6 @@
 
 #include "crypto_scalarmult_curve25519.h"
+#include "private/implementations.h"
 #include "scalarmult_curve25519.h"
 #include "runtime.h"
 
@@ -20,11 +21,11 @@ int
 crypto_scalarmult_curve25519(unsigned char *q, const unsigned char *n,
                              const unsigned char *p)
 {
-    size_t        i;
-    unsigned char d = 0;
+    size_t                 i;
+    volatile unsigned char d = 0;
 
     if (implementation->mult(q, n, p) != 0) {
-        return -1;
+        return -1; /* LCOV_EXCL_LINE */
     }
     for (i = 0; i < crypto_scalarmult_curve25519_BYTES; i++) {
         d |= q[i];
